34608cc5eb3adac2200596aeaf1433cfee4d8990
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-08-18 10:54:28 -0300

    exercicio4 completo

---
+++ Lista1/src/Exercicio4/Rio.java
@@ -0,0 +1,42 @@
+package Exercicio4;
+
+public class Rio {
+
+	public String nome;
+	public float nivel;
+	public boolean poluido;
+	
+	public Rio() {
+		
+	}
+	
+	public Rio(String n, float nvl, boolean p) {
+		this.nome = n;
+		this.nivel = nvl;
+		this.poluido = p;
+	}
+	
+	public void chover(float x) {
+		this.nivel += x;
+	}
+	
+	public void ensolarar(float x) {
+		this.nivel -= x;
+	}
+	
+	public void limpar() {
+		this.poluido = true;
+	}
+	
+	public void sujar() {
+		this.poluido = false;
+	}
+	
+	public void mostrar() {
+		System.out.println("Nome: "+this.nome);
+		System.out.println("Nivel: "+this.nivel);
+		System.out.println("Poluido: "+this.poluido);
+		System.out.println("------------------------------------------");
+	}
+
+}
---
+++ Lista1/src/Exercicio4/TestaRio.java
@@ -0,0 +1,28 @@
+package Exercicio4;
+
+public class TestaRio {
+
+	public static void main(String[] args) {
+		//Rio 1 - Usando construtor com paramentros
+		Rio rio1 = new Rio("Mar Vermelho", 5.4F, true);
+		
+		rio1.chover(15);
+		rio1.ensolarar(5);
+		rio1.limpar();
+		rio1.mostrar();
+		
+		//Rio 2 - Usando construtor sem parametros
+		Rio rio2 = new Rio();
+		
+		rio2.nome = "Rio Tigre";
+		rio2.nivel = 20;
+		rio2.poluido = false;
+		
+		rio2.chover(5);
+		rio2.ensolarar(10);
+		rio2.sujar();
+		rio2.mostrar();
+
+	}
+
+}
