0299d93137ebf073e104e9be34731749a47542be
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-08-18 10:20:40 -0300

    exercicio 3 completo

---
+++ Lista1/src/Exercicio3/Produto.java
@@ -0,0 +1,45 @@
+package Exercicio3;
+
+public class Produto {
+	
+	public int id;
+	public String descricao;
+	public int qtde;
+	public float preco;
+
+	public Produto(int id, String d, int q, float p) {
+		this.id = id;
+		this.descricao = d;
+		this.qtde = q;
+		this.preco = p;
+	}
+	
+	public Produto() {
+		
+	}
+	
+	public void comprar(int x) {
+		this.qtde += x;
+	}
+	
+	public void vender(int x) {
+		this.qtde -= x;
+	}
+	
+	public void subir(float x) {
+		this.preco += x;
+	}
+	
+	public void descer(float x) {
+		this.preco -= x;
+	}
+	
+	public void mostrar() {
+		System.out.println("ID: "+this.id);
+		System.out.println("Descrição: "+this.descricao);
+		System.out.println("Quantidade: "+this.qtde);
+		System.out.println("Preço: "+this.preco);
+		System.out.println("----------------------------------------------");
+	}
+
+}
---
+++ Lista1/src/Exercicio3/TestaProduto.java
@@ -0,0 +1,26 @@
+package Exercicio3;
+
+public class TestaProduto {
+
+	public static void main(String[] args) {
+		//Produto 1 - Usando construtor com parametros
+		Produto produto1 = new Produto(1, "batata", 10, 3.3F);
+		produto1.vender(3);
+		produto1.comprar(7);
+		produto1.mostrar();
+		
+		//Produto 2 - Usando Construtor sem parametros
+		Produto produto2 = new Produto();
+		
+		produto2.id = 2;
+		produto2.descricao = "doce";
+		produto2.qtde = 8;
+		produto2.preco = 5.3F;
+		
+		produto2.vender(3);
+		produto2.comprar(7);
+		produto2.mostrar();
+
+	}
+
+}
